1594143c43a2f3650afad3f4949de0baa5ea7109
brapha0060 <brapha0060@HAHW9Y6DXV1.inst.hcpss.org>
2019-03-25 11:51:26 -0400

    fized up

--- Online Store Git2/src/store/DeparmentPO.java
+++ Online Store Git2/src/store/DeparmentPO.java
@@ -1,12 +1,45 @@
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 
 public class DeparmentPO {
 
 	BorderPane root;
+	VBox top;
+	Label department;
+	HBox name, change;
 	
 	public DeparmentPO() {
 		root = new BorderPane();
+		startUp();
+	}
+	
+	public void startUp() {
+		
+		department = new Label();
+		department.setText("DEPARTMENT");
+		department.setStyle("-fx-font: 12 sansserif;");
+		
+		change = new HBox();
+		change.setStyle("-fx-background-color: BLUE;");
+		change.setAlignment(Pos.CENTER);
+		change.setPrefSize(1600, 80);
+		
+		name = new HBox();
+		name.setStyle("-fx-background-color: GREEN;");
+		name.setAlignment(Pos.CENTER);
+		name.setPrefSize(1600, 20);
+		name.getChildren().addAll(department);
+		
+		top = new VBox();
+		top.getChildren().addAll(change, name);
+		top.setPrefSize(1600, 100);
+		
+		root.setTop(top);
+		
 	}
 	
 	public Pane getRoot() {
