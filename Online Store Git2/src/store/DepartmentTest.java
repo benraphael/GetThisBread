23a6d368dc71ab187e707f3dcd53442414a6800f
brapha0060 <brapha0060@HAHW9Y6DXV1.inst.hcpss.org>
2019-03-25 10:36:11 -0400

    making department test

--- Online Store Git2/src/store/DepartmentTest.java
+++ Online Store Git2/src/store/DepartmentTest.java
@@ -1,9 +1,18 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-public class DepartmentTest {
+public class DepartmentTest extends Application{
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		launch(args);
 
+	}
+
+	@Override
+	public void start(Stage dep) throws Exception {
+		DeparmentPO depart = new DeparmentPO();
+		
+		
 	}
 
 }
