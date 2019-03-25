9b9b84e88606e9e38bf509434855cf2f7b7f4996
nraj1400 <nraj1400@HAHW9XNDXV1.inst.hcpss.org>
2019-03-25 11:59:35 -0400

    need help! center pane is not priting out error at like 50

--- Online Store Git2/src/store/CartName.java
+++ Online Store Git2/src/store/CartName.java
@@ -8,50 +8,24 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
-public class CartName extends Application{
+public class CartName extends Application {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		// System.out.println("figm");
 		launch(args);
-	}
-	private BorderPane root;
-	private ArrayList<Product> cartItem = new ArrayList<Product>();
-	
-	public CartName() {
-		root = new BorderPane();
-		createCart();
-	}
-	
-	private void createCart() {
-		HBox bottom = new HBox();
-		Button checkout = new Button("Go to Checkout");
-	Button goHome = new Button("Go to Home Page");
-		bottom.getChildren().addAll(checkout, goHome);
-		root.setBottom(bottom);
-		
-		cartPane organizer = new cartPane();
-		System.out.println("ger");
-		
-		
-		checkout.setOnAction(event -> System.out.println("test1")); //Change once all the classes are together
-		goHome.setOnAction(event -> System.out.println("test2")); //Change once all the classes are together
-	}
-	
-	public Pane getRoot() {
-		return root;
 	}
 
 	@Override
 	public void start(Stage stage) throws Exception {
-		System.out.println("nig");
-
-		cartPane organizer = new cartPane();
 		
-		Scene scene = new Scene(organizer.getRoot());
-		//Stage stage = new Stage();
+		
+		cartPane organizer = new cartPane();
+
+		Scene scene = new Scene(organizer.getRoot(), 1600, 800);
+		stage.setMaximized(true);
 		stage.setScene(scene);
 		stage.show();
-		stage.setResizable(false);
-		
+
 	}
 }
--- Online Store Git2/src/store/cartPane.java
+++ Online Store Git2/src/store/cartPane.java
@@ -1,6 +1,9 @@
+import java.util.ArrayList;
+
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Parent;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -11,43 +14,56 @@ import javafx.scene.layout.VBox;
 
 public class cartPane {
 	private BorderPane root;
-	private HBox top,center ,bottom;
+	private HBox top, center, bottom;
 	private Label cart;
-	
-	//implement scroll bar, arrays
+	private ArrayList<Product> cartItem = new ArrayList<Product>();
+	// implement scroll bar, arrays
+
 	public cartPane() {
-		System.out.println("nig");
 		root = new BorderPane();
-		root.setPrefSize(700, 600);
+		createCart();
 		run();
 	}
+
+	private void createCart() {
+		HBox bottom = new HBox();
+		Button checkout = new Button("Go to Checkout");
+		Button goHome = new Button("Go to Home Page");
+		bottom.getChildren().addAll(checkout, goHome);
+		root.setBottom(bottom);
+
+		checkout.setOnAction(event -> System.out.println("test1")); // Change once all the classes are together
+		goHome.setOnAction(event -> System.out.println("test2")); // Change once all the classes are together
+	}
+
 	private void run() {
-		System.out.println("nig");
-		Image image1 = new Image("/store/BetterAttemptAtLogo.png");
+		Image image1 = new Image("File: /store/BetterAttemptAtLogo.png");
 		ImageView imgView = new ImageView(image1);
-		
+
 		top = new HBox();
-		top.setStyle("-fx-background-color: Bisque");
+		top.setStyle("-fx-background-color: Bisque;");
 		top.setAlignment(Pos.CENTER);
-//		imgView.setFitHeight(200);
-//		imgView.setFitWidth(170);
+		imgView.setFitHeight(200);
+		imgView.setFitWidth(170);
 		top.getChildren().addAll(imgView);
 		
-		
+//		center.setStyle("-fx-background-color: BLUE;");
+//		center.setAlignment(Pos.CENTER);
+//		imgView.setFitHeight(100);
+//		imgView.setFitWidth(170);
+//		center.getChildren().addAll(imgView);
+
 		root.setTop(top);
 		root.setCenter(center);
 		root.setBottom(bottom);
-		
-		
-//		top.setPadding();
-//		top.setSpacing();
-		
+
+		// top.setPadding();
+		// top.setSpacing();
+
 	}
-	
+
 	public Pane getRoot() {
-		// TODO Auto-generated method stub
 		return root;
 	}
 
 }
-
