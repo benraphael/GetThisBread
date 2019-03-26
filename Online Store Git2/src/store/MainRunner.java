24f7cdf00a2d9e2a5f140142a42412088ce7475e
ibrown1799 <ibrown1799@HAHW7Y2BXV1.inst.hcpss.org>
2019-03-26 10:00:47 -0400

    Added loadDepartment method

--- Online Store Git2/src/store/MainRunner.java
+++ Online Store Git2/src/store/MainRunner.java
@@ -1,21 +1,70 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class MainRunner extends Application {
 
+	private static final String FILENAME = "";
+	
+	private ArrayList<Department> deps;
+	
 	public static void main(String[] args) {
 		launch(args);
 	}
 
 	@Override
 	public void start(Stage stage) throws Exception {
+		deps = new ArrayList<Department>();
+		loadDepartments(FILENAME);
 		stage.setTitle("https://LetsGetThisBread.com");
 		HomePagePO organizer = new HomePagePO();
 		Scene scene = new Scene(organizer.getRoot(), 1600, 800);
 		stage.setMaximized(true);
 		stage.setScene(scene);
 		stage.show();
+	}
+	
+	private void loadDepartments(String fileName) {
+		try {
+			Scanner scan = new Scanner(new File(fileName));
+			String jsonIn = "";
+			while (scan.hasNextLine()) {
+				jsonIn += scan.nextLine();
+			}
+			scan.close();
+			
+			JSONObject mainObject = new JSONObject(jsonIn);
+			JSONArray departments = mainObject.getJSONArray("departments");
+			
+			for (int i=0;i<departments.length();i++) {
+				Department dep = new Department(departments.getJSONObject(i).getString("name"));
+				ArrayList<Product> depProd = new ArrayList<Product>();
+				JSONArray products  = departments.getJSONObject(i).getJSONArray("products");
+				for (int j=0;j<products.length();j++) {
+					String name = products.getJSONObject(j).getString("name");
+					Double cost = products.getJSONObject(j).getDouble("cost");
+					String desc = products.getJSONObject(j).getString("description");
+					String url = products.getJSONObject(j).getString("imageURL");
+					Product tempProd = new Product(url, name, cost, desc);
+					depProd.add(tempProd);
+				}
+				dep.setProducts(depProd);
+				deps.add(dep);
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
