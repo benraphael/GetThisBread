6d989ee13672e6a68a41daf6edafeb1f83a2b48d
brapha0060 <brapha0060@HAHW9Y6DXV1.inst.hcpss.org>
2019-03-27 11:59:11 -0400

    NO SCROLLPANE

--- Online Store Git2/src/store/DeparmentPO.java
+++ Online Store Git2/src/store/DeparmentPO.java
@@ -1,73 +1,83 @@
+import java.awt.Component;
+import java.awt.ScrollPane;
+
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollBar;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.TilePane;
 import javafx.scene.layout.VBox;
 
 public class DeparmentPO {
 
 	BorderPane root;
-	VBox top, center;
+	VBox Top, center;
 	GridPane centerMain;
 	Label department;
 	HBox name, change;
 	
+
 	public DeparmentPO() {
 		root = new BorderPane();
 		startUp();
 	}
-	
+
 	public void startUp() {
-		
+
 		department = new Label();
 		department.setText("DEPARTMENT");
 		department.setStyle("-fx-font: 12 sansserif;");
-		
+
 		change = new HBox();
 		change.setStyle("-fx-background-color: BLUE;");
 		change.setAlignment(Pos.CENTER);
 		change.setPrefSize(1600, 80);
-		
+
 		name = new HBox();
 		name.setStyle("-fx-background-color: GREEN;");
 		name.setAlignment(Pos.CENTER);
 		name.setPrefSize(1600, 20);
 		name.getChildren().addAll(department);
-		
-		top = new VBox();
-		top.getChildren().addAll(change, name);
-		top.setPrefSize(1600, 100);
-		
+
+		Top = new VBox();
+		Top.getChildren().addAll(change, name);
+		Top.setPrefSize(1600, 100);
+
 		centerMain = new GridPane();
 		centerMain.setAlignment(Pos.CENTER);
+		centerMain.setHgap(5);
+		centerMain.setVgap(100);
+		centerMain.setPadding(new Insets(10, 10, 10, 10));
 
-		
-		for(int i = 0;i<10;i++) {
-			for(int g = 0;g<10;g++) {
-				Button button = new Button();
-				button.setPrefSize(20, 20);
-				centerMain.setRowIndex(button, i);
-				centerMain.setColumnIndex(button, g);
-				centerMain.getChildren().addAll(button);
+		for (int i = 0; i < 2; i++) {
+			for (int g = 0; g < 5; g++) {
+				VBox box = new VBox();
+				box.setPrefSize(200, 200);
+				box.setStyle("-fx-border-color: black;" + "-fx-border-insets: 5;" + "-fx-border-width: 3;" + "-fx-border-style: solid;");
+				centerMain.setRowIndex(box, i);
+				centerMain.setColumnIndex(box, g);
+				centerMain.getChildren().addAll(box);
 			}
 		}
-		
+
 		center = new VBox();
 		center.setStyle("-fx-background-color: BISQUE;");
 		center.setAlignment(Pos.CENTER);
-		center.setSpacing(5);
+		center.setSpacing(50);
 		center.getChildren().addAll(centerMain);
-		
-		root.setTop(top);
+
+		root.setTop(Top);
 		root.setCenter(center);
-		
+
 	}
-	
+
 	public Pane getRoot() {
 		return root;
 	}
-	
+
 }
