452f813e396f8aba0ffa2b07b86f68c8ff4403b2
ibrown1799 <ibrown1799@HAHW9XWCXV1.inst.hcpss.org>
2019-04-04 07:53:51 -0400

    Started work on checkout

--- Online Store Git2/src/store/Checkout.java
+++ Online Store Git2/src/store/Checkout.java
@@ -1,8 +1,11 @@
 
 import javafx.event.ActionEvent;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.Labeled;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
@@ -10,29 +13,42 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 
-public class Checkout {
+public class Checkout extends MainRunner {
 
 	private BorderPane root;
-	Label lbl, lbl2;
+	private Label name, address, subscript;
 	private Button order;
 	private Button cancel;
 	private RadioButton one, two, three;
-	private TextField imput1, imput2;
+	private TextField imputName, imputAddress;
 
 	public Checkout() {
 		
+		subscript = new Label();
+		subscript.setText("© 2019-2019, LetsGetThisBread.com, Inc. or its affiliates");
+		subscript.setStyle("-fx-text-fill: bisque");
+		
+		HBox bottom = new HBox();
+		bottom.setAlignment(Pos.BOTTOM_CENTER);
+		bottom.setSpacing(20);
+		bottom.setPadding(new Insets(20));
+		bottom.setStyle("-fx-background-color: #362204");
+		bottom.getChildren().add(subscript);
+		
+		VBox center = new VBox();
+		center.setPadding(new Insets(20));
+		center.setStyle("-fx-background-color: bisque");
+		
 		root = new BorderPane();
 
-		lbl = new Label();
-		lbl.setText("Name");
-		lbl2 = new Label();
-		lbl2.setText("Address");
+		name = new Label("Name");
+		address = new Label("Adress");
 
-		imput1 = new TextField();
-		imput1.setPrefWidth(10000);
+		imputName = new TextField();
+		imputName.setPrefWidth(500);
 
-		imput2 = new TextField();
-		imput2.setPrefWidth(10000);
+		imputAddress = new TextField();
+		imputAddress.setPrefWidth(500);
 
 		order = new Button();
 		order.setText("ORDER");
@@ -42,18 +58,27 @@ public class Checkout {
 		cancel.setText("CANCEL");
 		cancel.setOnAction(event -> buttonClick(event));
 
-		VBox pane = new VBox();
 		HBox one = new HBox();
+		one.setAlignment(Pos.CENTER);
+		one.setSpacing(20);
+		one.setPadding(new Insets(20));
+		
 		HBox two = new HBox();
-		pane.getChildren().addAll(one, two);
-		one.getChildren().addAll(lbl, imput1);
-		two.getChildren().addAll(lbl2, imput2);
+		two.setAlignment(Pos.CENTER);
+		two.setSpacing(20);		
+		two.setPadding(new Insets(20));
+		
+		one.getChildren().addAll(name, imputName);
+		two.getChildren().addAll(address, imputAddress);
+		center.getChildren().addAll(one, two);
 
+		root.setTop(new DepartmentBar().getRoot());
+		root.setBottom(bottom);
+		root.setCenter(center);
 	}
 
 	private void buttonClick(ActionEvent event) {
-		if (event.getSource() == order) {
-		}
+		System.out.println(((Button) event.getSource()).getText());
 	}
 
 	public Pane getRoot() {
